5db8ba8052cf5793dab602ad2cfa8c29864c99a1
arjunc007 <arjunchauhan007@gmail.com>
2018-10-20 17:44:02 +0100

    Added images for animation

--- polarQuest.js
+++ polarQuest.js
@@ -1,4 +1,26 @@
 /*eslint-env browser*/
+/*jslint devel: true */
+
+window.onload = function() {
+    "use strict";
+    var sources = {
+        resource1: "img/32 x 32 platform character_idle_0.png",
+        resource2: "img/32 x 32 platform character_idle_1.png",
+        resource3: "img/32 x 32 platform character_idle_2.png",
+        resource4: "img/32 x 32 platform character_idle_3.png",
+        resource5: "32 x 32 platform character_jump_0.png",
+        resource6: "img/32 x 32 platform character_jump_1.png",
+        resource7: "img/32 x 32 platform character_run_0.png",
+        resource8: "img/32 x 32 platform character_run_1.png",
+        resource9: "img/32 x 32 platform character_run_2.png",
+        resource10: "img/32 x 32 platform character_run_3.png",
+        resource11: "img/32 x 32 platform character_run_4.png",
+        resource12: "img/32 x 32 platform character_run_5.png"
+    };
+    loadImages(sources);
+};
+
+var loadComplete = false;
 
 var width = 800;
 var height = 600;
@@ -29,13 +51,17 @@ function Point(x, y) {
     this.y = y;
 }
 
+var totalFood = 1000;
+var totalFuel = 1000;
+var vehicles = ["Dog sled", "Motor Sled", "Truck"];
+
 function Player(x, y) {
     "use strict";
     this.pos = new Point(x, y);
     this.color = "#FA5858";
     this.height = 30;
     this.width = 10;
-    this.speed = 20;
+    this.speed = 50;
     this.jumpSpeed = 50;
     this.velocity = new Point(0, 0);
     this.isJumping = false;
@@ -59,13 +85,19 @@ function Player(x, y) {
         this.pos.y += Math.min(groundHeight, this.velocity.y * dt);
         
         if (this.isJumping) {
-            this.velocity.y += 30 * dt;
+            this.velocity.y += 50 * dt;
         }
         
         if (this.pos.y > groundHeight) {
             this.velocity.y = 0;
             this.pos.y = groundHeight;
             this.isJumping = false;
+        }
+        
+        if (this.pos.x < 0) {
+            this.pos.x = 0;
+        } else if (this.pos.x > width - this.width) {
+            this.pos.x = width - this.width;
         }
     };
     
@@ -76,23 +108,32 @@ function Player(x, y) {
 }
 
 var player = new Player(20, groundHeight);
+var now, dt, last = timestamp();
+
+var currentFood, currentFuel, currentVehicle;
 
 function update(dt) {
     "use strict";
     player.update(dt);
+    if (currentVehicle === "Truck") {
+        currentFuel -= 20 * dt;
+    }
+    currentFood -= 10 * dt;
+    
+    if (currentFood < 0 || currentFuel < 0) {
+        console.log("Expedition failed");
+    }
 }
 
 function draw() {
     "use strict";
     ctx.fillStyle = "#2EFEF7";
     ctx.fillRect(0, 0, width, groundHeight);
-    ctx.fillStyle = "#FFFFFF";
+    ctx.fillStyle = "#FAFAFA";
     ctx.fillRect(0, groundHeight, width, height);
     
     player.show();
 }
-
-var now, dt, last = timestamp();
 
 //run the game loop
 function frame() {
@@ -105,8 +146,51 @@ function frame() {
     requestAnimationFrame(frame);
 }
 
+function startGame(food, fuel, vehicle) {
+    "use strict";
+    currentFood = food;
+    currentFuel = fuel;
+    currentVehicle = vehicle;
+    
+    while (!loadComplete) {/*do nothing*/}
+    
+    requestAnimationFrame(frame);
+}
+
+function startScreen() {
+    "use strict";
+    var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "white");
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(10, 10, width, height);
+    requestAnimationFrame(startScreen);
+}
+
 //start game
-requestAnimationFrame(frame);
+requestAnimationFrame(startScreen);
+
+function loadImages(sources) {
+    "use strict";
+    var images = {};
+    var loadedImages = 0;
+    var numImages = 0;
+    for(var src in sources) {
+        numImages++;
+    }
+    for (src in sources) {
+        images[src] = new Image();
+        images[src].onload = function() {
+            if(++loadedImages >= numImages) {
+                loadComplete = true;
+            }
+            images[src].src = sources[src];
+        }
+    }
+    
+}
 
 document.addEventListener('keydown', function (e) {
     "use strict";
@@ -121,11 +205,11 @@ document.addEventListener('keydown', function (e) {
 
 document.addEventListener('keyup', function (e) {
     "use strict";
-    if (e.keyCode === 65) {   //A
+    if (e.keyCode === 65 || e.keyCode === 37) {   //A
         leftKey = false;
-    } else if (e.keyCode === 68) {  //D
+    } else if (e.keyCode === 68 || e.keyCode === 39) {  //D
         rightKey = false;
-    } else if (e.keyCode === 87) {   //W
+    } else if (e.keyCode === 87 || e.keyCode === 38) {   //W
         jumpKey = false;
     }
 });
