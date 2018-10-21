a85620345729622ecc49fac5e6ae180768284f8b
arjunc007 <arjunchauhan007@gmail.com>
2018-10-21 10:25:41 +0100

    Added animations

--- polarQuest.js
+++ polarQuest.js
@@ -8,14 +8,20 @@ window.onload = function() {
         resource2: "img/32 x 32 platform character_idle_1.png",
         resource3: "img/32 x 32 platform character_idle_2.png",
         resource4: "img/32 x 32 platform character_idle_3.png",
-        resource5: "32 x 32 platform character_jump_0.png",
+        resource5: "img/32 x 32 platform character_jump_0.png",
         resource6: "img/32 x 32 platform character_jump_1.png",
-        resource7: "img/32 x 32 platform character_run_0.png",
-        resource8: "img/32 x 32 platform character_run_1.png",
-        resource9: "img/32 x 32 platform character_run_2.png",
-        resource10: "img/32 x 32 platform character_run_3.png",
-        resource11: "img/32 x 32 platform character_run_4.png",
-        resource12: "img/32 x 32 platform character_run_5.png"
+        resource7: "img/run_right_0.png",
+        resource8: "img/run_right_1.png",
+        resource9: "img/run_right_2.png",
+        resource10: "img/run_right_3.png",
+        resource11: "img/run_right_4.png",
+        resource12: "img/run_right_5.png",
+        resource13: "img/run_left_0.png",
+        resource14: "img/run_left_1.png",
+        resource15: "img/run_left_2.png",
+        resource16: "img/run_left_3.png",
+        resource17: "img/run_left_4.png",
+        resource18: "img/run_left_5.png"
     };
     loadImages(sources);
 };
@@ -54,14 +60,49 @@ var totalFood = 1000;
 var totalFuel = 1000;
 var vehicles = ["On Foot", "Sled & Dogs", "Truck", "Motorbike"];
 
+var idleImages = [];
+var jumpImages = [];
+var runImages = [];
+var animTimer = 0;
+
+function Animation(imgList) {
+    this.images = imgList;
+    this.totalFrames = imgList.length;
+    this.currentFrame = 0;
+}
+
+Animation.prototype.animate = function () {
+    this.currentFrame++;
+    if(this.currentFrame >= this.totalFrames) {
+        this.currentFrame = 0;
+    }
+}
+Animation.prototype.getImage = function() {
+    return this.images[this.currentFrame];
+}
+
 function Player(x, y) {
     "use strict";
+    this.currentAnim;
+    this.animations = {};
     this.pos = new Point(x, y);
     this.color = "#FA5858";
-    this.height = 30;
-    this.width = 10;
+    this.height = 40;
+    this.width = 40;
     this.velocity = new Point(0, 0);
     this.isJumping = false;
+    this.speed;
+    this.jumpSpeed;
+
+    this.animate = function() {
+        if(this.isJumping) {
+            this.image = this.animations["jump"].getImage();
+        } else if (this.velocity.x === 0) {
+            this.image = this.animations["idle"].getImage();
+        } else {
+            this.image = this.animations["run"].getImage();
+        }
+    }
     
     this.jump = function () {
         if (!this.isJumping) {
@@ -71,18 +112,50 @@ function Player(x, y) {
     };
     
     this.update = function (dt) {
+
+        if(this.isJumping) {
+            this.currentAnim = this.animations["jump"];
+        } else if (this.velocity.x < 0) {
+            this.currentAnim = this.animations["runLeft"];
+        } else if (this.velocity.x > x){
+            this.currentAnim = this.animations["runRight"];
+        } else {
+            this.currentAnim = this.animations["idle"];
+        }
+
+
         if (leftKey) {
-            this.pos.x -= this.speed * dt;
+            this.velocity.x = -this.speed;
         } else if (rightKey) {
-            this.pos.x += this.speed * dt;
+            this.velocity.x = this.speed;
         }
         if (jumpKey) {
             this.jump();
         }
-        this.pos.y += Math.min(groundHeight, this.velocity.y * dt);
+        
+        animTimer += dt;
+        
+        if(animTimer > 0.25) {
+            this.currentAnim.animate();
+            animTimer = 0;
+        }
         
         if (this.isJumping) {
-            this.velocity.y += 30 * dt;
+            this.pos.x += this.velocity.x * dt;
+            this.pos.y += this.velocity.y * dt;
+            this.velocity.y += 60 * dt;
+        } else {
+            if(this.velocity.x > 0) {
+                this.image = idleImages[0];
+            } else if(this.velocity.x < 0) {
+                this.image = idleImages[0];
+            } else {
+                this.image = idleImages[0];   
+            }
+            this.pos.x += this.velocity.x * dt;
+            this.pos.y += this.velocity.y * dt;
+            this.velocity.x *= 0.9;
+            this.velocity.y *= 0.9;
         }
         
         if (this.pos.y > groundHeight) {
@@ -93,8 +166,13 @@ function Player(x, y) {
     };
     
     this.show = function () {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.pos.x, this.pos.y - this.height, this.width, this.height);
+        if(this.currentAnim) {
+            ctx.drawImage(this.currentAnim.getImage(), this.pos.x, this.pos.y - this.height, this.width, this.height);
+        }
+        else {
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.pos.x, this.pos.y - this.height, this.width, this.height);
+        }
     };
 }
 
@@ -164,9 +242,17 @@ function startgame() {
             break;
         }
     
-    //while (!loadComplete) {/*do nothing*/}
-    
-    requestAnimationFrame(frame);
+    start();
+}
+
+function start() {
+    if(loadComplete) {
+        requestAnimationFrame(frame);
+    }
+    else {
+        console.log("Still Loading Images");
+        requestAnimationFrame(start);
+    }
 }
 
 function startScreen() {
@@ -186,19 +272,24 @@ function loadImages(sources) {
     var images = {};
     var loadedImages = 0;
     var numImages = 0;
-    for(var src in sources) {
+    for (var src in sources) {
         numImages++;
-    }
-    for (src in sources) {
         images[src] = new Image();
         images[src].onload = function() {
             if(++loadedImages >= numImages) {
+                var idleImages = [images.resource1, images.resource2, images.resource3, images.resource4];
+                player.animations["idle"] = new Animation(idleImages);
+                var jumpImages = [images.resource5, images.resource6];
+                player.animations["jump"] = new Animation(jumpImages);
+                var runImages = [images.resource7, images.resource8, images.resource9, images.resource10, images.resource11, images.resource12];
+                player.animations["runRight"] = new Animation(runImages);
+                var runImages = [images.resource13, images.resource14, images.resource15, images.resource16, images.resource17, images.resource18];
+                player.animations["runLeft"] = new Animation(runImages);
                 loadComplete = true;
             }
-            images[src].src = sources[src];
-        }
+        };
+        images[src].src = sources[src];
     }
-    
 }
 
 document.addEventListener('keydown', function (e) {
